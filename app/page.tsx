fd88ad814a0ffa0fdec9fc245ad8240b95cc5655
fckolob <131497920+fckolob@users.noreply.github.com>
2025-11-20 20:14:39 -0300

    conection to seeded database

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { fetchProducts } from './lib/data';
 
 export default function Page() {
+  fetchProducts();
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
